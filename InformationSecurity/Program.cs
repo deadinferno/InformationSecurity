254e2bdc8fa6a62e117acffffb556ac7c53473a1
Vladimir <38225120+deadinferno@users.noreply.github.com>
2018-09-28 12:57:52 +0300

    апнгш

--- InformationSecurity/Program.cs
+++ InformationSecurity/Program.cs
@@ -1,8 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/* 
+10.	Зашифровать и расшифровать сообщение, содержащее любые символы кодовой 
+таблицы ASCII, с помощью шифра Вижинера. Длина ключа должна быть не менее 5 символов. 
+ */
+
+/*
+ Тесты
+
+ // объявляем массив строк
+ string[] arr = new string[] {};
+
+ */
+
+using System;
 
 namespace InformationSecurity
 {
@@ -10,6 +19,19 @@ namespace InformationSecurity
     {
         static void Main(string[] args)
         {
+
+            String str = "абвгдежзийклмнопрстуфхцчшщъыьэюя";
+            string[] table = new string[str.Length];
+
+            table[0] = "иди нахуй";
+            table[1] = "мудила ты бля";
+            table[3] = "ууууу тупые дефки";
+
+            //string[] a = new string[str.Length];
+
+            String input;
+            System.Console.Write("Введите строку\nВВОД: ");
+            input = System.Console.ReadLine();
 
         }
     }
